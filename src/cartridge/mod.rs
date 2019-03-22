52572eca334e1d58fd13bdd211b448e0ea5ef2c1
ESTBLC <estblcsk@gmail.com>
2019-03-22 01:26:50 +0000

    Refactor CARTRIDGE

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -3,6 +3,12 @@ use std::path;
 
 mod mbc0;
 
+enum CartridgeTypes {
+    MBC0 = 0x00,
+}
+
+const CARTRIDGE_TYPE_BYTE: usize = 0x147;
+
 pub trait Cartridge {
     fn read_rom(&self, addr: u16) -> u8;
     fn read_ram(&self, addr: u16) -> u8;
@@ -14,8 +20,14 @@ pub trait Cartridge {
 pub fn new(path: &path::Path) -> Box<Cartridge> {
     let rom = fs::read(path).unwrap();
 
-    match rom[0x147] {
-        0x00 => return Box::new(mbc0::Mbc0::new(rom)),
+    match get_cartridge_type(rom[CARTRIDGE_TYPE_BYTE]) {
+        CartridgeTypes::MBC0 => Box::new(mbc0::Mbc0::new(rom)),
+    }
+}
+
+fn get_cartridge_type(type_byte: u8) -> CartridgeTypes {
+    match type_byte {
+        0x00 => CartridgeTypes::MBC0,
         _ => panic!("Not a Gameboy rom file !"),
     }
 }
